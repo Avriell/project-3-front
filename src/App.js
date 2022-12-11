96e8609cf8ca54ecba00d127479d3e3d27e8e739
Eyan Cea <eyan.moses@gmail.com>
2022-12-11 13:15:32 -0600

    Final push before CSS

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import Edit from './Components//Edit.js'
 function App() {
 
   //  const [display, setDisplay] = useState(false)
-   const [movie, setMovie] = useState([])
+   const [movies, setMovies] = useState([])
 
   //Get...Calling on Movie.js file
-  const getMovie = () => {
-     axios.get('http://localhost:3000/movielist')
-     .then((response) => setMovie(response.data), (err) => console.log(err))
+  const getMovies = () => {
+     axios.get('http://localhost:3000/movierec')
+     .then((response) => setMovies(response.data), (err) => console.log(err))
      .catch((error) => console.log(error))
   }
   
@@ -24,52 +24,52 @@ function App() {
   //Create...Calling on Add.js file
   const handleCreate = (data) => {
     console.log(data)
-     axios.post('http://localhost:3000/movielist', data)
+     axios.post('http://localhost:3000/movierec', data)
      .then((response) => {
-        setMovie([...movie, response.data])
+        setMovies([...movies, response.data])
         
      })
   }
 
   //Edit
   const handleEdit = (data) => {
-    axios.put('http://localhost:3000/movielist/' + data._id, data)
+    axios.put('http://localhost:3000/movierec/' + data._id, data)
     .then((response) => {
-       let newMovie = movie.map((movie) => {
-        return movie._id !== data._id ? movie : data
+       let newMovies = movies.map((movies) => {
+        return movies._id !== data._id ? movies : data
        })
-       setMovie(newMovie)
+       setMovies(newMovies)
     })
  }
 
  //Delete
-  const handleDelete = (deletedMovie) => {
-     axios.delete('http://localhost:3000/movielist/' + deletedMovie._id)
+  const handleDelete = (deletedMovies) => {
+     axios.delete('http://localhost:3000/movierec/' + deletedMovies._id)
      .then((response) => {
-      let newMovie = movie.filter((movie) => {
-        return movie._id !== response._id
+      let newMovies = movies.filter((movies) => {
+        return movies._id !== response._id
        })
-      setMovie(newMovie)
-      getMovie()
+      setMovies(newMovies)
+      getMovies()
      })
   }
 
   useEffect(() => {
-     getMovie()
+     getMovies()
   }, [])
 
   return (
     <div className="container">
       
       <div className="header">
-      <header className>Movie Recommendations</header>
+      <header>Movie Recommendations</header>
       </div>
 
       {/* <button onClick={() => setDisplay(!display)}>Add New Movie</button> */}
 
       <Add handleCreate = {handleCreate}/> 
 
-      {movie.map((movie) => {
+      {movies.map((movie) => {
         return (
           <div>
             <Movie movie = {movie}/>
